0679bd94a2be75c5a4d2240a24ae466a0c6edad7
SamuraiT <t.yasukawa01@gmail.com>
2015-08-10 00:09:42 +0900

    Add examples

--- chapter4/multimode.py
+++ chapter4/multimode.py
@@ -8,8 +8,8 @@ def plot_multimode(m,layout=net.spring_layout, type_string='type', with_labels=T
     colors=['r','g','b','c','m','y','k']
     colormap={}
     d=net.degree(m)  #we use degree for sizing nodes
-    pos=layout(m)  #compute layout 
-    
+    pos=layout(m)  #compute layout
+
     #Now we need to find groups of nodes that need to be colored differently
     nodesets=defaultdict(list)
     for n in m.nodes():
@@ -19,25 +19,25 @@ def plot_multimode(m,layout=net.spring_layout, type_string='type', with_labels=T
             ##this happens if a node doesn't have a type_string -- give it a None value
             t='None'
         nodesets[t].append(n)
-        
+
     ## Draw each group of nodes separately, using its own color settings
-    print "drawing nodes..."
+    print("drawing nodes...")
     i=0
     for key in nodesets.keys():
         ns=[d[n]*100 for n in nodesets[key]]
         net.draw_networkx_nodes(m,pos,nodelist=nodesets[key], node_size=ns, node_color=colors[i], alpha=0.6)
         colormap[key]=colors[i]
         i+=1
-        if i==len(colors): 
+        if i==len(colors):
             i=0  ### wrap around the colormap if we run out of colors
-    print colormap  
-    
+    print(colormap)
+
     ## Draw edges using a default drawing mechanism
-    print "drawing edges..."
-    net.draw_networkx_edges(m,pos,width=0.5,alpha=0.5)  
-    
-    print "drawing labels..."
-    if with_labels: 
+    print("drawing edges...")
+    net.draw_networkx_edges(m,pos,width=0.5,alpha=0.5)
+
+    print("drawing labels...")
+    if with_labels:
         net.draw_networkx_labels(m,pos,font_size=12)
     plot.axis('off')
     if filename_prefix is not '':
